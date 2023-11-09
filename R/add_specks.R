925a2e6ceea10ee2ea72b73d7a966ddd41851d2a
awunderground <aaronrwilliams100@gmail.com>
2023-11-09 00:53:01 -0500

    Fix error in add_specks() introduced from old code

--- R/add_specks.R
+++ R/add_specks.R
@@ -15,16 +15,13 @@ add_specks <- function(discrimination) {
     dplyr::filter(.data$.source_label == "synthetic") %>%
     dplyr::pull(".pred_synthetic")
   
-  # Compare propensity score by comparing ECDFs using KS Distance --------------------------------------------------------
-  # Generate ECDF for original and synthetic data
-  ecdf_original <- stats::ecdf(propensities_original)
-  ecdf_synthetic <- stats::ecdf(propensities_synthetic)
-  
-  # Sequence of values to extract ECDFs
-  z <- seq(from = 0, to = 1, by = 0.0001)
-  
   # Calculate KS Distance of the original and synthetic ECDFS
-  specks <- suppressWarnings(stats::ks.test(ecdf_original(z), ecdf_synthetic(z), exact = FALSE))$statistic
+  specks <- suppressWarnings(
+    stats::ks.test(
+      propensities_original, 
+      propensities_synthetic, 
+      exact = FALSE)$statistic
+    )
   
   specks <- unname(specks)
   
