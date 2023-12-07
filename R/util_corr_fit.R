ac10a715fb388f46721e44f059f34c80ee30d398
emcfalls <emm113@duke.edu>
2023-12-07 14:48:28 -0500

    updated util_corr_fit outcome

--- R/util_corr_fit.R
+++ R/util_corr_fit.R
@@ -31,8 +31,9 @@ util_corr_fit <- function(postsynth, data) {
   synthetic_data <- dplyr::select_if(synthetic_data, is.numeric)
   data <- dplyr::select_if(data, is.numeric)
 
-  # reorder data names
+  # reorder data names (this appears to check if the variables are the same)
   data <- dplyr::select(data, names(synthetic_data))
+  
   
   # helper function to find a correlation matrix with the upper tri set to zeros
   lower_triangle <- function(x) {
@@ -49,48 +50,94 @@ util_corr_fit <- function(postsynth, data) {
     return(correlation_matrix)
   }
   
-  # find the lower triangle of the original data linear correlation matrix
-  original_lt <- lower_triangle(data)
+  # find the lower triangle of the original data linear correlation matrix and return a df
+  original_lt <- data.frame(lower_triangle(data))
   
-  # find the lower triangle of the synthetic data linear correlation matrix
-  synthetic_lt <- lower_triangle(synthetic_data)
+  # adding variable 2 column to the original df
+  original_lt$var2 <- colnames(original_lt)
   
-  # compare names
-  if (any(rownames(original_lt) != rownames(synthetic_lt))) {
-    stop("ERROR: rownames are not identical")
-  }
+  # restructuring the correlation matrix so the cols are var1, var2, original
+  original_lt <- original_lt %>%
+    pivot_longer(cols = !var2, names_to = "var1", values_to = "original") %>%
+    filter(!is.na(original)) %>%
+    arrange(var1) %>%
+    select(var1, var2, original)
   
-  if (any(colnames(original_lt) != colnames(synthetic_lt))) {
-    stop("ERROR: colnames are not identical")
-  }
+  # find the lower triangle of the synthetic data linear correlation matrix and return a df
+  synthetic_lt <- data.frame(lower_triangle(synthetic_data))
   
-  # find the difference between the matrices
-  difference_lt <- synthetic_lt - original_lt
+  # adding variable 2 column to the synthetic df
+  synthetic_lt$var2 <- colnames(synthetic_lt)
+  
+  # restructuring the correlation matrix so the cols are var1, var2, synthetic
+  synthetic_lt <- synthetic_lt %>%
+    pivot_longer(cols = !var2, names_to = "var1", values_to = "synthetic") %>%
+    filter(!is.na(synthetic)) %>%
+    arrange(var1) %>%
+    select(var1, var2, synthetic)
+  
+  # find the difference between the original correlations and the synthetic 
+  correlation_data <- original_lt %>%
+    left_join(synthetic_lt, by = c("var1","var2")) %>%
+    mutate(difference = original - synthetic,
+           proportion_difference = .data$difference / .data$original)
   
   # find the length of the nonzero values in the matrices
-  n <- choose(ncol(difference_lt), 2)
-  
+  n <- choose(ncol(correlation_data), 2)
+
   # calculate the correlation fit and divide by n
-  correlation_fit <- sqrt(sum(difference_lt ^ 2, na.rm = TRUE)) / n
+  correlation_fit <- sqrt(sum(correlation_data$difference ^ 2, na.rm = TRUE)) / n
   
-  difference_vec <- as.numeric(difference_lt)[!is.na(difference_lt)]
+  difference_vec <- as.numeric(correlation_data$difference)
   
   # mean absolute error
   correlation_difference_mae <- difference_vec %>%
     abs() %>%
     mean()
-  
+
   # root mean square error
-  correlation_difference_rmse <- 
-    difference_vec ^ 2%>%
+  correlation_difference_rmse <- difference_vec ^ 2 %>%
     mean() %>%
     sqrt()
+
+  
+   # compare names
+  # if (any(rownames(original_lt) != rownames(synthetic_lt))) {
+  #   stop("ERROR: rownames are not identical")
+  # }
+  # 
+  # if (any(colnames(original_lt) != colnames(synthetic_lt))) {
+  #   stop("ERROR: colnames are not identical")
+  # }
+  # 
+  # # find the difference between the matrices
+  # difference_lt <- synthetic_lt - original_lt
+  # 
+  # # find the length of the nonzero values in the matrices
+  # n <- choose(ncol(difference_lt), 2)
+  
+  # calculate the correlation fit and divide by n
+  # correlation_fit <- sqrt(sum(difference_lt ^ 2, na.rm = TRUE)) / n
+  # 
+  # difference_vec <- as.numeric(difference_lt)[!is.na(difference_lt)]
+  # 
+  # # mean absolute error
+  # correlation_difference_mae <- difference_vec %>%
+  #   abs() %>%
+  #   mean()
+  # 
+  # # root mean square error
+  # correlation_difference_rmse <- 
+  #   difference_vec ^ 2%>%
+  #   mean() %>%
+  #   sqrt()
   
   return(
     list(
-      correlation_original = original_lt,
-      correlation_synthetic = synthetic_lt,
-      correlation_difference = difference_lt,
+      correlation_data = correlation_data,
+      # correlation_original = original_lt,
+      # correlation_synthetic = synthetic_lt
+      # correlation_difference = difference_lt,
       correlation_fit = correlation_fit,
       correlation_difference_mae = correlation_difference_mae,
       correlation_difference_rmse = correlation_difference_rmse
