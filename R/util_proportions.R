0e9d3efb80c206530f3b547a4c1665c10d15641f
awunderground <aaronrwilliams100@gmail.com>
2023-12-19 18:07:48 -0500

    Add some crazy changes to allow variable selection in util_proportions()

--- R/util_proportions.R
+++ R/util_proportions.R
@@ -4,34 +4,107 @@
 #' @param data A data frame with the original data
 #' @param weight_var An unquoted name of a weight variable
 #' @param group_by An unquoted name of a (or multiple) grouping variable(s)
+#' @param common_vars A logical for if only common variables should be kept
+#' @param synth_vars A logical for if only synthesized variables should be kept
 #'
 #' @return A tibble with variables, classes, and relative frequencies
 #' 
 #' @export
 #'
-util_proportions <- function(postsynth, data, weight_var = 1, 
-                             group_by = NULL) {
+util_proportions <- function(postsynth, 
+                             data, 
+                             weight_var = NULL, 
+                             group_by = NULL,
+                             common_vars = TRUE,
+                             synth_vars = TRUE) {
   
   
-  if ("postsynth" %in% class(postsynth)) {
+  if (is_postsynth(postsynth)) {
     
     synthetic_data <- postsynth$synthetic_data
     
+    variable_order <- 
+      levels(postsynth$jth_synthesis_time$variable)
+  
   } else {
     
     synthetic_data <- postsynth
     
   }
   
+  # the goal is to create .temp_weight that is equal to 1 if weight_var isn't 
+  # specified and equal to the weight if weight_var is a variable
+  synthetic_weight <- synthetic_data %>%
+    dplyr::select({{ weight_var }}) %>%
+    dplyr::mutate(.temp_weight = {{ weight_var }}) %>%
+    dplyr::select(-{{ weight_var }})
+  
+  # if {{ weight }} is NULL then set the weight to 1
+  if (ncol(synthetic_weight) == 0) {
+    
+    synthetic_weight <- synthetic_data %>%
+      dplyr::mutate(.temp_weight = 1) %>%
+      dplyr::select(".temp_weight")
+    
+  }
+  
+  data_weight <- data %>%
+    dplyr::select({{ weight_var }}) %>%
+    dplyr::mutate(.temp_weight = {{ weight_var }}) %>%
+    dplyr::select(-{{ weight_var }})
+  
+  # if {{ weight }} is NULL then set the weight to 1
+  if (ncol(data_weight) == 0) {
+    
+    data_weight <- data_weight %>%
+      dplyr::mutate(.temp_weight = 1) %>%
+      dplyr::select(".temp_weight")
+    
+  }
+  
+  # combine the weight variable df to the synthetic data
+  synthetic_data <- synthetic_data %>%
+    dplyr::bind_cols(synthetic_weight) %>%
+    dplyr::select(-{{ weight_var }})
+  
+  # combine the weight variable df to the confidential data
+  data <- data %>%
+    dplyr::bind_cols(data_weight) %>%
+    dplyr::select(-{{ weight_var }})
+  
+  # filter to only synthesized variables
+  # keep group_by variables
+  if (is_postsynth(postsynth) & synth_vars) {
+    
+    synthetic_data <- synthetic_data %>%
+      dplyr::select(dplyr::all_of(variable_order), {{ group_by }}, ".temp_weight")
+    
+    data <- data %>%
+      dplyr::select(dplyr::all_of(variable_order), {{ group_by }}, ".temp_weight")
+    
+  }
+  
+  # only keep variables in both data sets
+  # keep group_by variables
+  if (common_vars) {
+    
+    common_vars <- intersect(names(data), names(synthetic_data))
+    
+    data <- data %>%
+      dplyr::select(dplyr::all_of(common_vars), {{ group_by }}, ".temp_weight")
+    
+    synthetic_data <- synthetic_data %>%
+      dplyr::select(dplyr::all_of(common_vars), {{ group_by }}, ".temp_weight")
+    
+  }
+  
   # dropping columns that are numeric (excluding the weight variable)
   synthetic_data <- synthetic_data %>%
-    dplyr::mutate(.temp_weight = {{ weight_var }}) %>%
     dplyr::select(tidyselect::where(is.factor), 
                   tidyselect::where(is.character), 
                   ".temp_weight")
   
   data <- data %>%
-    dplyr::mutate(.temp_weight = {{ weight_var }}) %>%
     dplyr::select(tidyselect::where(is.factor), 
                   tidyselect::where(is.character), 
                   ".temp_weight")
