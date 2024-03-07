2eb90f239f4bed7982abe3732fe6279db2de5a1b
VISION-CA <75582215+MortonC78483@users.noreply.github.com>
2024-03-06 22:45:46 -0800

    add grouping functionality

--- R/add_discriminator_auc.R
+++ R/add_discriminator_auc.R
@@ -4,18 +4,19 @@
 #' added using add_propensities())
 #' @param split A logical for if the metric should be calculated separately for 
 #' the training/testing split. Defaults to TRUE.
-#'
+#' @param group A list of variable names to group by
+#' 
 #' @return A discrimination object with propensities (likely added using 
 #' add_propensities()) with discriminator AUC
 #' 
 #' @export
 #'
-add_discriminator_auc <- function(discrimination, split = TRUE) {
+add_discriminator_auc <- function(discrimination, group = c(),split = TRUE) {
   
   if (split) {
     
     discriminator_auc <- discrimination$propensities %>%
-      dplyr::group_by(.data$.sample) %>%
+      dplyr::group_by(across(all_of(c(".sample", group))))%>%
       yardstick::roc_auc(".source_label", ".pred_synthetic") %>%
       dplyr::mutate(.sample = factor(.data$.sample, levels = c("training", "testing"))) %>%
       dplyr::arrange(.data$.sample)
@@ -23,6 +24,7 @@ add_discriminator_auc <- function(discrimination, split = TRUE) {
   } else {
     
     discriminator_auc <- discrimination$propensities %>%
+      dplyr::group_by(across(all_of(group)))%>%
       yardstick::roc_auc(".source_label", ".pred_synthetic") %>%
       dplyr::mutate(.sample = factor("overall", levels = "overall"))
     
--- R/add_specks.R
+++ R/add_specks.R
@@ -3,31 +3,46 @@
 #' @param discrimination A discrimination with added propensities
 #' @param split A logical for if the metric should be calculated separately for 
 #' the training/testing split. Defaults to TRUE.
+#' @param group A list of variable names to group by
 #'
 #' @family Utility metrics
 #'
 #' @return A discrimination with SPECKS
 #' 
 #' @export
-add_specks <- function(discrimination, split = TRUE) {
+add_specks <- function(discrimination, group = c(), split = TRUE) {
   
   calc_specks <- function(propensities) {
     
     propensities_original <- propensities %>%
+      dplyr::group_by(across(all_of(group))) %>%
       dplyr::filter(.data$.source_label == "original") %>%
+      dplyr::summarize(".pred_synthetic" = list(c(.data$.pred_synthetic))) %>%
       dplyr::pull(".pred_synthetic")
     
     propensities_synthetic <- propensities %>%
+      dplyr::group_by(across(all_of(group))) %>%
       dplyr::filter(.data$.source_label == "synthetic") %>%
+      dplyr::summarize(".pred_synthetic" = list(c(.data$.pred_synthetic))) %>%
       dplyr::pull(".pred_synthetic")
     
+    specks_func <- function(original, synthetic){
+      suppressWarnings(
+        stats::ks.test(
+          original, 
+          synthetic, 
+          exact = FALSE)$statistic
+      )
+    }
     # Calculate KS Distance of the original and synthetic ECDFS
-    specks <- suppressWarnings(
-      stats::ks.test(
-        propensities_original, 
-        propensities_synthetic, 
-        exact = FALSE)$statistic
-    )
+    #specks <- suppressWarnings(
+    # stats::ks.test(
+    #    propensities_original, 
+    #    propensities_synthetic, 
+    #    exact = FALSE)$statistic
+    #)
+    # calculate the observed pMSE
+    specks <- mapply(specks_func, propensities_original, propensities_synthetic)
     
     specks <- unname(specks)
     
@@ -45,25 +60,52 @@ add_specks <- function(discrimination, split = TRUE) {
       dplyr::filter(.data$.sample == "testing") %>%
       calc_specks()
     
-    specks <- tibble::tibble(
-      .source = factor(c("training", "testing"), levels = c("training", "testing")),
-      .specks = c(specks_training, specks_testing)
-    )
+    if (length(group)==0){ # original case
+      specks <- tibble::tibble(
+        .source = factor(c("training", "testing"), levels = c("training", "testing")),
+        .specks = c(specks_training, specks_testing)
+      )
+    }
+    else{ # we have passed a list of grouping variables
+      groups <- discrimination$propensities %>%
+        dplyr::group_by(across(all_of(group))) %>%
+        group_keys()
+      groups <- rbind(groups, groups) # make 2 copies for train/test
+      
+      specks <- tibble::tibble(
+        groups,
+        .source = factor(c(rep("training", length(specks_training)), rep("testing", length(specks_testing))), levels = c("training", "testing")),
+        .specks = c(specks_training, specks_testing)
+      )
+    }
     
   } else {
     
     specks_overall <- discrimination$propensities %>%
       calc_specks()
     
-    specks <- tibble::tibble(
-      .source = factor("overall", levels = "overall"),
-      .specks = specks_overall
-    )
+    if (length(group)==0){ # original case
+      specks <- tibble::tibble(
+        .source = factor("overall", levels = "overall"),
+        .specks = specks_overall
+      )
+    }
+    else{ # we have passed a list of grouping variables
+      groups <- discrimination$propensities %>%
+        dplyr::group_by(across(all_of(group))) %>%
+        group_keys()
+      
+      specks <- tibble::tibble(
+        groups,
+        .source = factor(c(rep("overall", length(specks_overall))), levels = c("overall")),
+        .specks = specks_overall
+      )
+    }
     
   }
   
   discrimination$specks <- specks
   
   return(discrimination)
-
+  
 }
