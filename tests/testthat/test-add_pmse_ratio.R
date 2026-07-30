c7430a154c5717509dc4fd5f1c3a224859dd8dbc
Thiyaghessan <thiyaghessan@gmail.com>
2026-07-30 11:42:45 -0400

    parallelize add_pmse_ratio() bootstrap with furrr, closes #113
    This commit does the following:
    
    * the furrr refactor of R/add_pmse_ratio.R
    * a reproducibility test plus updated exact values in test-add_pmse_ratio.R
    * furrr to Imports and future to Suggests in DESCRIPTION
    * the NEWS.md entry under the development version header
    * the regenerated man/add_pmse_ratio.Rd

--- tests/testthat/test-add_pmse_ratio.R
+++ tests/testthat/test-add_pmse_ratio.R
@@ -41,7 +41,7 @@ test_that("add_pmse returns ideal value for identical data with variation " , {
   
   expect_equal(round(disc$pmse$.pmse, digit = 1), 0)
   # this is a bad test but will at least tell us when the code logic changes
-  expect_equal(round(disc$pmse$.pmse_ratio, 5), 0.56812)
+  expect_equal(round(disc$pmse$.pmse_ratio, 5), 0.59907)
   
   disc <- disc %>%
     add_pmse() %>%
@@ -49,8 +49,64 @@ test_that("add_pmse returns ideal value for identical data with variation " , {
   
   expect_equal(round(disc$pmse$.pmse, digit = 2), c(0, 0))
   # this is a bad test but will at least tell us when the code logic changes
-  expect_equal(round(disc$pmse$.pmse_ratio, 5), c(0.27469, 0.67145))
+  expect_equal(round(disc$pmse$.pmse_ratio, 5), c(0.28372, 0.62333))
   
+})
+
+test_that("add_pmse_ratio is reproducible and plan-independent", {
+
+  skip_if_not_installed("future")
+
+  data <-
+    data.frame(
+      x = rnorm(n = 200, mean = 0, sd = 1),
+      y = rnorm(n = 200, mean = 0, sd = 1)
+    )
+
+  postsynth <-
+    list(
+      synthetic_data = data,
+      jth_synthesis_time = data.frame(
+        variable = factor(c("x", "y"))
+      )
+    ) %>%
+    structure(class = "postsynth")
+
+  ed <- eval_data(conf_data = data, synth_data = postsynth)
+
+  dt_mod <- parsnip::decision_tree() %>%
+    parsnip::set_mode(mode = "classification") %>%
+    parsnip::set_engine(engine = "rpart")
+
+  rec <- recipes::recipe(.source_label ~ ., data = discrimination(ed)$combined_data)
+
+  disc <- suppressWarnings(
+    discrimination(ed) %>%
+      add_propensities(
+        recipe = rec,
+        spec = dt_mod
+      ) %>%
+      add_pmse(split = FALSE)
+  )
+
+  # sequential, twice with the same seed
+  set.seed(20260730)
+  seq_run1 <- add_pmse_ratio(disc, split = FALSE, times = 5)
+
+  set.seed(20260730)
+  seq_run2 <- add_pmse_ratio(disc, split = FALSE, times = 5)
+
+  expect_equal(seq_run1$pmse$.null_pmse, seq_run2$pmse$.null_pmse)
+
+  # parallel plan with the same seed must match the sequential result
+  future::plan(future::multisession, workers = 2)
+  on.exit(future::plan(future::sequential), add = TRUE)
+
+  set.seed(20260730)
+  par_run <- add_pmse_ratio(disc, split = FALSE, times = 5)
+
+  expect_equal(seq_run1$pmse$.null_pmse, par_run$pmse$.null_pmse)
+
 })
 
 test_that("add_pmse returns perfect value for identical data without variation " , {
