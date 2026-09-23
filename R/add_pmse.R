cdf730ff0b3057415d434b243e807c2841939567
nikhitaairi <53191228+nikhitaairi@users.noreply.github.com>
2026-09-23 12:47:55 -0400

    Added grouping variable to add_pmse.R without causing regression

--- R/add_pmse.R
+++ R/add_pmse.R
@@ -1,69 +1,102 @@
-#' Add pMSE to discrimination object
+#' Add pMSE (propensity score mean squared error) to discrimination object
 #'
-#' @param discrimination A discrimination object with propensities (likely 
+#' @param discrimination A discrimination object with propensities (likely
 #' added using add_propensities())
-#' @param split A logical for if the metric should be calculated separately for 
+#' @param split A logical for if the metric should be calculated separately for
 #' the training/testing split. Defaults to TRUE.
+#' @param group_by_q An optional grouping variable to calculate the pMSE within each group.
 #'
-#' @return A discrimination object with propensities (likely added using 
+#' @return A discrimination object with propensities (likely added using
 #' add_propensities()) with a pMSE
-#' 
+#'
 #' @family Utility metrics
-#' 
+#'
 #' @export
-#' 
-add_pmse <- function(discrimination, split = TRUE) {
-  
+#'
+add_pmse <- function(discrimination, split = TRUE, group_by_q = NULL) {
+
   calc_pmse <- function(propensities) {
-    
+
     # calculate the expected propensity
-    prop_synthetic <- propensities %>%
+    prop_synthetic <- propensities |>
       dplyr::summarize(
         n_synthetic = sum(.data$.source_label == "synthetic"),
         n_total = dplyr::n()
-      ) %>%
-      dplyr::mutate(prop_synthetic = .data$n_synthetic / .data$n_total) %>%
+      ) |>
+      dplyr::mutate(prop_synthetic = .data$n_synthetic / .data$n_total) |>
       dplyr::pull("prop_synthetic")
-    
-    propensities_vec <- propensities %>%
+
+    propensities_vec <- propensities |>
       dplyr::pull(".pred_synthetic")
-    
+
     # calculate the observed pMSE
     pmse <- mean((propensities_vec - prop_synthetic) ^ 2)
-    
+
     return(pmse)
-    
+
   }
-  
+
   if (split) {
-    
-    pmse_training <- discrimination$propensities %>%
-      dplyr::filter(.data$.sample == "training") %>%
-      calc_pmse()
-    
-    pmse_testing <- discrimination$propensities %>%
-      dplyr::filter(.data$.sample == "testing") %>%
-      calc_pmse()
-    
-    pmse <- tibble::tibble(
-      .source = factor(c("training", "testing"), levels = c("training", "testing")),
-      .pmse = c(pmse_training, pmse_testing)
-    )
-    
+
+    if (!is.null(group_by_q)) {
+
+      propensities_by <- split(
+        discrimination$propensities,
+        c(
+          discrimination$propensities[[group_by_q]],
+          discrimination$propensities$.sample
+        )
+      )
+      pmse_list <- lapply(propensities_by, calc_pmse)
+      pmse <- tibble::tibble(
+        .source = factor(names(pmse_list), levels = names(pmse_list)),
+        .pmse = unlist(pmse_list)
+      )
+
+    } else {
+
+      # create list of dataframes by split (training/testing)
+      propensities_by <- split(discrimination$propensities, discrimination$propensities$.sample)
+      pmse_list <- lapply(propensities_by, calc_pmse)
+
+      pmse <- tibble::tibble(
+        .source = factor(c("training", "testing"), levels = c("training", "testing")),
+        .pmse = c(pmse_list$training, pmse_list$testing)
+      )
+
+    }
+
   } else {
-    
-    pmse_overall <- discrimination$propensities %>%
+
+    if (!is.null(group_by_q)) {
+
+      propensities_by <- split(
+        discrimination$propensities,
+        discrimination$propensities[[group_by_q]]
+      )
+      pmse_list <- lapply(propensities_by, calc_pmse)
+      pmse <- tibble::tibble(
+        .source = factor(names(pmse_list), levels = names(pmse_list)),
+        .pmse = unlist(pmse_list)
+      )
+
+    } else {
+
+      pmse_overall <- discrimination$propensities |>
       calc_pmse()
-    
-    pmse <- tibble::tibble(
-      .source = factor("overall", levels = "overall"),
-      .pmse = pmse_overall
-    )
-    
+
+      pmse <- tibble::tibble(
+        .source = factor("overall", levels = "overall"),
+        .pmse = pmse_overall
+      )
+
+    }
+
+
   }
-  
+
   discrimination$pmse <- pmse
-  
+
   return(discrimination)
-  
+
 }
