916e7aa4fd3221286cf5ca9ee68e034d00c62119
nikhitaairi <53191228+nikhitaairi@users.noreply.github.com>
2026-09-23 12:37:00 -0400

    Added group to add_discriminator_auc, tested, documented

--- R/add_discriminator_auc.R
+++ R/add_discriminator_auc.R
@@ -1,36 +1,67 @@
 #' Add discriminator AUC to discrimination object
 #'
-#' @param discrimination A discrimination object with propensities (likely 
+#' @param discrimination A discrimination object with propensities (likely
 #' added using add_propensities())
-#' @param split A logical for if the metric should be calculated separately for 
+#' @param split A logical for if the metric should be calculated separately for
 #' the training/testing split. Defaults to TRUE.
+#' @param group_by_q An optional grouping variable to calculate the discriminator AUC within each group.__
 #'
-#' @return A discrimination object with propensities (likely added using 
-#' add_propensities()) with discriminator AUC
-#' 
+#' @return A discrimination object with propensities (likely added using
+#' add_propensities()) with discriminator AUC. When split is TRUE or when group_by_q is
+#' provided, the discriminator AUC will be calculated within each group and the returned table
+#' contains a ".group" and/or ".sample" column as appropriate.
+#'
 #' @export
 #'
-add_discriminator_auc <- function(discrimination, split = TRUE) {
-  
+add_discriminator_auc <- function(discrimination, split = TRUE, group_by_q = NULL) {
+
   if (split) {
-    
-    discriminator_auc <- discrimination$propensities %>%
-      dplyr::group_by(.data$.sample) %>%
-      yardstick::roc_auc(".source_label", ".pred_synthetic") %>%
-      dplyr::mutate(.sample = factor(.data$.sample, levels = c("training", "testing"))) %>%
-      dplyr::arrange(.data$.sample) %>%
+
+    if  (!is.null(group_by_q)) {
+      discriminator_auc <- discrimination$propensities |>
+        dplyr::group_by(.data[[group_by_q]], .data$.sample)
+    } else {
+      discriminator_auc <- discrimination$propensities |>
+        dplyr::group_by(.data$.sample)
+    }
+
+    discriminator_auc <- discriminator_auc |>
+      yardstick::roc_auc(".source_label", ".pred_synthetic")
+
+    if (!is.null(group_by_q)) {
+      discriminator_auc <- discriminator_auc |>
+        dplyr::rename(".group" := !!rlang::sym(group_by_q))
+    }
+
+    discriminator_auc <- discriminator_auc |>
+      dplyr::mutate(.sample = factor(.data$.sample, levels = c("training", "testing"))) |>
+      dplyr::arrange(.data$.sample) |>
       dplyr::ungroup()
-    
+
   } else {
-    
-    discriminator_auc <- discrimination$propensities %>%
-      yardstick::roc_auc(".source_label", ".pred_synthetic") %>%
+
+    if (!is.null(group_by_q)) {
+      discriminator_auc <- discrimination$propensities |>
+        dplyr::group_by(.data[[group_by_q]])
+    } else {
+      discriminator_auc <- discrimination$propensities
+    }
+
+    discriminator_auc <- discriminator_auc |>
+      yardstick::roc_auc(".source_label", ".pred_synthetic")
+
+    if (!is.null(group_by_q)) {
+      discriminator_auc <- discriminator_auc |>
+        dplyr::rename(".group" := !!rlang::sym(group_by_q))
+    }
+
+    discriminator_auc <- discriminator_auc |>
       dplyr::mutate(.sample = factor("overall", levels = "overall"))
-    
+
   }
-  
+
   discrimination$discriminator_auc <- discriminator_auc
-  
+
   return(discrimination)
-  
+
 }
--- tests/testthat/test-add_discriminator_auc.R
+++ tests/testthat/test-add_discriminator_auc.R
@@ -1,7 +1,7 @@
 test_that("add_discriminator_auc returns perfect value for identical data (no split) " , {
-  
+
   set.seed(1)
-  
+
   data <-
     data.frame(
       x = rnorm(n = 1000, mean = 0, sd = 1),
@@ -16,36 +16,36 @@ test_that("add_discriminator_auc returns perfect value for identical data (no sp
       )
     ) %>%
     structure(class = "postsynth")
-  
+
   ed <- eval_data(conf_data = data, synth_data = postsynth)
-  
+
   logistic_mod <- parsnip::logistic_reg() %>%
     parsnip::set_mode(mode = "classification") %>%
     parsnip::set_engine(engine = "glm")
-  
+
   rec <- recipes::recipe(.source_label ~ ., data = discrimination(ed)$combined_data)
-  
+
   disc <- discrimination(ed) %>%
     add_propensities(
       recipe = rec,
       spec = logistic_mod
     ) %>%
     add_discriminator_auc(split = FALSE)
-  
+
   expect_equal(disc$discriminator_auc$.estimate, 0.5)
-  
+
 })
 
-test_that("add_discriminator_auc returns perfect value for identical data (split) " , {
-  
+test_that("add_discriminator_auc returns perfect value for identical data (split)", {
+
   set.seed(1)
-  
+
   data <-
     data.frame(
       x = rep(1, 1000),
       y = rep(1, 1000)
     )
-  
+
   postsynth <-
     list(
       synthetic_data = data,
@@ -54,15 +54,15 @@ test_that("add_discriminator_auc returns perfect value for identical data (split
       )
     ) %>%
     structure(class = "postsynth")
-  
+
   ed <- eval_data(conf_data = data, synth_data = postsynth)
-  
+
   logistic_mod <- parsnip::logistic_reg() %>%
     parsnip::set_mode(mode = "classification") %>%
     parsnip::set_engine(engine = "glm")
-  
+
   rec <- recipes::recipe(.source_label ~ ., data = discrimination(ed)$combined_data)
-  
+
   disc <- suppressWarnings(
     discrimination(ed) %>%
       add_propensities(
@@ -71,12 +71,54 @@ test_that("add_discriminator_auc returns perfect value for identical data (split
       ) %>%
       add_discriminator_auc()
   )
-  
+
   expect_equal(disc$discriminator_auc$.estimate, c(0.5, 0.5))
-  
+
 })
 
-test_that("add_pmse returns perfect value for seperable data (no split)" , {
+test_that("add_discriminator_auc returns perfect value for identical data (grouped and split)", {
+
+  set.seed(1)
+
+  data <-
+    data.frame(
+      x = rep(1, 1000),
+      y = rep(1, 1000),
+      g = rep(c("A", "B"), each = 500)
+    )
+
+  postsynth <-
+    list(
+      synthetic_data = data,
+      jth_synthesis_time = data.frame(
+        variable = factor(c("x", "y", "g"))
+      )
+    ) %>%
+    structure(class = "postsynth")
+
+  ed <- eval_data(conf_data = data, synth_data = postsynth)
+
+  logistic_mod <- parsnip::logistic_reg() %>%
+    parsnip::set_mode(mode = "classification") %>%
+    parsnip::set_engine(engine = "glm")
+
+  rec <- recipes::recipe(.source_label ~ ., data = discrimination(ed)$combined_data)
+
+  disc <- suppressWarnings(
+    discrimination(ed) %>%
+      add_propensities(
+        recipe = rec,
+        spec = logistic_mod
+      ) %>%
+      add_discriminator_auc(group_by_q = "g")
+  )
+
+  expect_equal(disc$discriminator_auc$.estimate, c(0.5, 0.5, 0.5, 0.5))
+  expect_true(".group" %in% names(disc$discriminator_auc))
+
+})
+
+test_that("add_pmse returns perfect value for separable data (no split)", {
   
   set.seed(1)
   
